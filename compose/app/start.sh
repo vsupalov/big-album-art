ae01b5b43bca991b8d8309949083e72ad12a8d89
Vladislav Supalov <v.supalov@gmail.com>
2017-06-29 11:49:37 +0200

    Just in case.

--- compose/app/start.sh
+++ compose/app/start.sh
@@ -7,4 +7,7 @@ virtualenv -q -p /usr/bin/python3.5 /srv/env
 source /srv/env/bin/activate
 pip install -r requirements.txt
 
+# nuke the db
+#FLASK_APP=main.py flask resetdb
+
 gunicorn -w 4 -b 0.0.0.0:8000 main:app
