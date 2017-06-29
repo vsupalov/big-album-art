534b1144774a08570073a6a41e8b6ab080ac8ff1
Vladislav Supalov <v.supalov@gmail.com>
2017-06-29 11:13:13 +0200

    Adjust gunicorn path.

--- compose/app/start.sh
+++ compose/app/start.sh
@@ -7,4 +7,4 @@ virtualenv -q -p /usr/bin/python3.5 /srv/env
 source /srv/env/bin/activate
 pip install -r requirements.txt
 
-gunicorn -w 4 -b 0.0.0.0:8000 baa.main:app
+gunicorn -w 4 -b 0.0.0.0:8000 main:app
