53c1fb89e2294afb7da08ed6c7eb4266cdf7bf2c
Vladislav Supalov <v.supalov@pivii.co>
2017-10-22 15:33:03 +0200

    Oh. That's how.

--- baa/main.py
+++ baa/main.py
@@ -131,7 +131,7 @@ def go_to_spotify():
 @app.route("/logout/")
 @login_required
 def logout():
-    if current_user.is_authenticated():
+    if current_user.is_authenticated:
         logout_user()
 
         log_info = {
@@ -146,7 +146,7 @@ def logout():
 
 @app.route("/")
 def start():
-    if not current_user.is_authenticated():
+    if not current_user.is_authenticated:
         d = {}
         redirect_url = get_spotify_login_link()
         d["spotify_link"] = redirect_url.url
